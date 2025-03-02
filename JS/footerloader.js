e0d452b784d9acddc47278a572285f47243eac4c
Soumya Purani <soumyapurani@gmail.com>
2025-03-02 14:26:22 -0400

    separated footer and built a footer loader

---
+++ JS/footerloader.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('footer.html')
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById('footer-container').innerHTML = html;
+      });
+  });
